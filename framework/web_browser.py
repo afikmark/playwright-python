852e24210e6b835a6181705d76dd88ddd5765237
afikm <afikmark@gmail.com>
2024-03-10 18:50:00 +0200

    removed WebBrowser class

--- framework/web_browser.py
+++ framework/web_browser.py
@@ -1,27 +1,5 @@
-from playwright.sync_api import sync_playwright, Browser
-from settings import ROOT_DIR
-
 
 class BrowserType:
     CHROME: str = 'chrome'
     FIREFOX: str = 'firefox'
-
-
-def _create_browser(browser_type: str, **kwargs) -> Browser:
-    match browser_type:
-        case BrowserType.FIREFOX:
-            browser = sync_playwright().start().firefox.launch(**kwargs)
-        case _:
-            browser = sync_playwright().start().chromium.launch(**kwargs)
-
-    return browser
-
-
-class WebBrowser:
-    def __init__(self, browser_type: str, **kwargs):
-        self.browser = _create_browser(browser_type, **kwargs)
-        self.page = self.browser.new_context(record_video_dir=f"{ROOT_DIR}/videos").new_page()
-
-    def close_browser(self) -> None:
-        self.browser.close()
 
