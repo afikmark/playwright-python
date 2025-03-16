35be6d9df2fcafb54e1534b01ad394abb546bdba
afikmark <afikmark@gmail.com>
2025-03-16 12:56:36 +0200

    replaced pytest built-in assertion with playwright expect

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,8 +1,11 @@
 import pytest
+import allure
+from tests import expect
 
 
+@allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.swag_ui
-def test_login(swag_ui, app_config, reporter):
+def test_login(swag_ui, app_config, reporter, page):
     """
     This test validates the login functionality
     of the application.
@@ -13,14 +16,17 @@ def test_login(swag_ui, app_config, reporter):
     login_page.login(app_config.user_name, app_config.user_password)
     inventory_page = swag_ui.inventory_page
     reporter.step('Step', 'Assert user is in inventory page after login')
-    assert inventory_page.is_in_page, 'not in inventory page'
+    expect(page).to_have_url(inventory_page.url)
 
 
 # @pytest.mark.skip("This test is for testing the video and screenshot capture on failure")
-def test_login_fail(swag_ui, app_config, reporter):
+def test_login_fail(swag_ui, app_config, reporter, page):
+    """
+    This test verifies login with invalid credentials
+    """
     login_page = swag_ui.login_page
     login_page.open()
     reporter.step('Step', 'Login to SwagLabs')
     login_page.login(user_name='wrong', password='password')
     inventory_page = swag_ui.inventory_page
-    assert inventory_page.is_in_page, 'not in inventory page'
+    expect(page).to_have_url(inventory_page.url)
