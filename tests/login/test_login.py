48e4abfe7c7c33c88fd4fe3c330ae72339399e02
afikmark <afikmark@gmail.com>
2025-03-21 16:19:54 +0200

    remove test_fail

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -18,16 +18,3 @@ def test_login(swag_ui, reporter, page, user_name, password):
     inventory_page = swag_ui.inventory_page
     reporter.step('Step', 'Assert user is in inventory page after login')
     expect(page).to_have_url(inventory_page.url)
-
-
-# @pytest.mark.skip("This test is for testing the video and screenshot capture on failure")
-def test_login_fail(swag_ui, app_config, reporter, page):
-    """
-    This test verifies login with invalid credentials
-    """
-    login_page = swag_ui.login_page
-    login_page.open()
-    reporter.step('Step', 'Login to SwagLabs')
-    login_page.login(user_name='wrong', password='password')
-    inventory_page = swag_ui.inventory_page
-    expect(page).to_have_url(inventory_page.url)
